835d8cfed287c40ba4551e4227e5471d07b2ecc1
collin178u <alexcollin03@orange.fr>
2023-06-06 15:52:21 +0200

    modification de LancerRaytracer.java

--- src/LancerRaytracer.java
+++ src/LancerRaytracer.java
@@ -12,7 +12,7 @@ public class LancerRaytracer {
     public static void main(String args[]){
 
         // Le fichier de description de la scène si pas fournie
-        String fichier_description="simple.txt";
+        String fichier_description="src/simple.txt";
 
         // largeur et hauteur par défaut de l'image à reconstruire
         int largeur = 512, hauteur = 512;
@@ -47,7 +47,8 @@ public class LancerRaytracer {
         Instant debut = Instant.now();
         System.out.println("Calcul de l'image :\n - Coordonnées : "+x0+","+y0
                            +"\n - Taille "+ largeur + "x" + hauteur);
-        Image image = scene.compute(x0, y0, l, h);
+        Image part1 = scene.compute(x0, y0, l/2, h/2);
+        Image part2 = scene.compute(l/2,h/2,l/2,h/2);
         Instant fin = Instant.now();
 
         long duree = Duration.between(debut, fin).toMillis();
@@ -55,6 +56,8 @@ public class LancerRaytracer {
         System.out.println("Image calculée en :"+duree+" ms");
         
         // Affichage de l'image calculée
-        disp.setImage(image, x0, y0);
+        disp.setImage(part1, x0, y0);
+        disp.setImage(part2,l/2,h/2);
+
     }	
 }
