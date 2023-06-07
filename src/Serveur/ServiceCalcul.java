790af0a729d2db0c0eecbf9b4ed1422192b1d116
Egario-akier <thierry.dissidia@gmail.com>
2023-06-07 15:07:06 +0200

    Correction ServiceCalcul

--- src/Serveur/ServiceCalcul.java
+++ src/Serveur/ServiceCalcul.java
@@ -1,9 +1,11 @@
 package Serveur;
 
+import raytracer.Image;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ServiceCalcul extends Remote {
 
-    void compute(int x0, int y0, int l, int h) throws RemoteException;
+    Image compute(int x0, int y0, int l, int h) throws RemoteException;
 }
