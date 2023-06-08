380a823582b973856557d9ab7e80eef953851b74
Egario-akier <thierry.dissidia@gmail.com>
2023-06-08 15:57:34 +0200

    Serveur.java

---
+++ src/Serveur/Serveur.java
@@ -0,0 +1,37 @@
+package Serveur;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Serveur {
+    public static void main(String[] args) {
+        int port = 1099;
+        if(args.length > 0) port = Integer.parseInt(args[0]);
+
+        Registry registry = null;
+        try {
+            registry = LocateRegistry.getRegistry(port);
+        } catch (Exception e) {
+            System.out.println("Erreur : " + e.getMessage());
+            System.exit(1);
+        }
+
+        ServiceDistributeur distributeur = null;
+        try {
+            distributeur = (ServiceDistributeur) UnicastRemoteObject.exportObject(new Distributeur(), port);
+        } catch (RemoteException e) {
+            System.out.println("Erreur : " + e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            registry.rebind("Distributeur", distributeur);
+        } catch (Exception e) {
+            System.out.println("Erreur : " + e.getMessage());
+            System.exit(1);
+        }
+
+    }
+}
