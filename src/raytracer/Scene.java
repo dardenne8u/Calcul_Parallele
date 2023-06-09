206bc5c102fd1f960d99b31e99bec81263f11bd1
Egario54 <thierry.dissidia@gmail.com>
2023-06-09 08:43:27 +0200

    Implement dans Scene.java

--- src/raytracer/Scene.java
+++ src/raytracer/Scene.java
@@ -1,8 +1,11 @@
 package raytracer;
 import java.util.*;
+
+import Serveur.ServiceCalcul;
+
 import java.io.*;
 
-public class Scene implements Serializable{
+public class Scene implements Serializable, ServiceCalcul{
 
     private static float EPSILON = 1E-3f;
     private HashSet<Primitive> objets;
