3e8a7f3d598d890c0f1924e0de825b8f28256153
Egario54 <thierry.dissidia@gmail.co>
2023-06-15 11:01:58 +0200

    modification script bash

--- src/lancerNoeud.sh
+++ src/lancerNoeud.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ssh 100.64.80."$0" "scp -r ../src \$(whoami)@100.64.80.$0:/var/tmp && cd /var/tmp/src/Serveur && java LancerNoeud "$1" 1099 &"
+utilisateur=$(whoami);
+scp -r ../../Calcul_Parallele $utilisateur@100.64.80.$0:/var/tmp
+ssh 100.64.80."$0" "cd /var/tmp/Calcul_Parallele/src/Serveur && java LancerNoeud "$1" 1099 &"
